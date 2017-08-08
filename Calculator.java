c9d847d68a2fd3fb059ade81a1d7a918306b831d
DaBinHan <darebit.han@gmail.com>
2017-08-08 14:53:30 +0900

    sum 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,14 @@ package com.mycompany.test;
 public class Calculator{
 
   public static vod main(String[] args){
-
+    int a = 4;
+    int b = 2;
+    System.out.println( sum(a,b) );
 
   }
+
+  private static int sum(int a, int b){
+    return a + b;
+  }
+
 }
